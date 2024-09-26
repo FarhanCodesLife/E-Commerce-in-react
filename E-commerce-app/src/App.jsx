c73939b6d887210e8eb798b4d76c7838a21eb856
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-09-26 17:07:19 +0500

    half work done

--- E-commerce-app/src/App.jsx
+++ E-commerce-app/src/App.jsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './component/Navbar'
+import { useDispatch, useSelector } from 'react-redux'
+import axios from 'axios'
+import { addtocart } from './config/redux/reducer/CartSlice'
 
 const App = () => {
+
+  let [products,setproducts]=useState(null)
+
+  useEffect(()=>{
+    let addtocartitems = ()=>{
+      
+        axios('https://dummyjson.com/products')
+        .then(res=>{
+          // console.log(res.data.products);
+          setproducts(res.data.products)
+          
+        })
+        .catch(err=>{console.log(err);
+        })
+        
+        
+      }
+      addtocartitems()
+    },[])
+    
+    console.log(products);
+  let selector = useSelector(state=>state.cart.cartitems)
+  console.log(selector);
+
+  let dispatch = useDispatch()
+  
+  let addcart =(item)=>{
+    dispatch(
+      addtocart({
+        item
+      })
+    )
+  }
+
+
   return (
 <>
 <Navbar/>
-<h1>app</h1>
+<div className='flex justify-center items-center flex-wrap gap-5 m-5'>
+
+{products? products.map((item)=>{
+  return <div className="card bg-base-100 w-72 shadow-xl">
+  <figure>
+    <img
+      src={item.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      {item.title.slice(0,12)+"..."}
+      <div className="badge badge-secondary">NEW</div>
+    </h2>
+    <p>{item.description.slice(0,80)+"..."}</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">{item.category}</div>
+      <div className="badge badge-outline">Products</div>
+    </div>
+    <div className='text-center  mt-3 btn btn-primary text-lg'>
+      <button onClick={()=>addcart(item)}>Add to cart</button>
+    </div>
+  </div>
+</div>
+
+}):<h1>loading...</h1> }
+</div>
+
 </>
 
 )
